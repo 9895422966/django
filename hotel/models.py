6f8de936b499ebc97296e09fdff3a371068a4858
9895422966 <thansheer10@gmail.com>
2021-03-25 19:08:20 +0530

    second commit

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class register:
+    name:str
+    email: str
+    phone:int
+    length:int
+    persons:int
--- hotel/views.py
+++ hotel/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def register(request):
+   if request.method =='POST': 
+      name=request.POST['name']
+      email=request.POST['email']
+      phone=request.POST['phone']
+      length=request.POST['length']
+      person=request.POST['person']
+      date=request.POST['date']
+   else:
+      return render(request,'register.html')
